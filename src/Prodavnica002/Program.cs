81d2100789362c365c8ec22c10f16001c63138ab
Aleksa Ristic <aristiccitsira@gmail.com>
2025-01-04 12:27:48 +0100

    Stage

---
+++ src/Prodavnica002/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Prodavnica002;
+
+class Program
+{
+    // Ova aplikacija je kopirana Prodavnica001
+    // (bez onoga sto si ti napisao kao zadatak)
+    // A onda sam dodao stvari neke.
+    // To znaci da i u ovoj aplikaciji imamo sve iste stvari kao u prethodnoj + nesto novo
+    static void Main(string[] args)
+    {
+        // Zadatak:
+        // Instanciraj List<Proizvod> i dodaj
+        // nekoliko proizvoda koristeci .Add() metodu klase List<>
+        
+        // ===== Kod ispod izmedju ovih komentara ===== (koristi koliko god hoces linija, samo da budu izmedju):
+        
+        // ======
+        
+        // Sada kada imamo listu proizvoda (to je nasa baza),
+        // mozemo da ispisemo sve proizvode uz pomoc foreach petlje
+        // Ne zaboravi da za ispisivanje mozes koristiti metodu iz Proizvod klase
+        
+        // ===== Kod ispod izmedju ovih komentara ===== (koristi koliko god hoces linija, samo da budu izmedju):
+        
+        // ======
+    }
+}
