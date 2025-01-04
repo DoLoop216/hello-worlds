902b095a21134d743bb9f89dd023b178c1ca3a47
Aleksa Ristic <aristiccitsira@gmail.com>
2025-01-04 12:45:59 +0100

    Stage

---
+++ README.md
@@ -0,0 +1,27 @@
+Iscitaj sve do kraja, a ne samo ono sto ti se svidja.
+
+Otvaraj projekte jedan po jedan.
+Nemoj ici napred nazad i raditi na vise projekata odjednom.
+Ako neki projekat zapne, nemoj prelaziti na drugi,
+jer sledeci zahteva da si naucio nesto iz prethodnog.
+
+Obavezno prati redosled projekata koji glasi:
+- Kalkulator
+- KalkulatorNapredni
+- KalkulatorNapredni1
+- KalkulatorNapredniFinal
+- KalkulatorReturn
+- Prodavnica001
+- Prodavnica002
+- Prodavnica003
+- Prodavnica004
+- AutoPlac001
+
+U projektima je objasnjeno sta treba da uradis.
+Ako treba da pises kod, napisano je.
+
+Poenta je da razumes svaku mogucu rec koju vidis ispred sebe u fajlu,
+osim reci koje se nalaze u linijama koje pocinju sa
+ - namespace
+
+Sve ostale reci (syntax-a) mimo ovoga bi trebalo da razumes.
---
+++ src/AutoPlac001/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace AutoPlac001;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Zadatak:
+        // Kreiraj klasu Automobil koja ce da predstavlja jedan automobil
+        // Dodaj svojstva: Marka, Model, Godiste, Cena
+        // Kreiraj metodu unutar te klase koja ce da ispisuje sve podatke o automobilu
+        // u konzoli
+        
+        // Nakon sto si ovo uradio, obrisi komentare
+        // IZNAD i daj marku/stefanu/aleksi da uradi zadatak ISPOD:
+        
+        // Zadatak:
+        // Imas tip Automobil. Kreiraj praznu listu automobila (List<Automobil>)
+        // U novoj linij dodaj 3 automobila sa proizvoljnim podacima
+        // koristeci .Add() metodu liste
+        // Nakon toga, koristeci foreach petlju, ispisite sve automobile.
+        // Obrati paznju, unutar klase Automobil imas metodu koja ispisuje sve podatke
+        // da ne moras rucno da ispisujes
+    }
+}
